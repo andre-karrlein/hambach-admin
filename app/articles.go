6f13af1d22ec0e2024a1b07a49e1f7f966a2cccf
André Karrlein <andre.karrlein@karrlein.com>
2021-05-29 12:38:16 +0000

    add articles component

--- app/articles.go
+++ app/articles.go
@@ -5,3 +5,23 @@ import "github.com/maxence-charriere/go-app/v8/pkg/app"
 type articles struct {
 	app.Compo
 }
+
+func (a *articles) Render() app.UI {
+	return app.Div().Body(
+		&navbar{},
+		app.Section().Class("section").Body(
+			app.Body().Class("has-navbar-fixed-top").Body(
+				app.Div().Class("container").Body(
+					app.Div().Class("columns").Body(
+						app.Div().Class("column is-one-fifth").Body(
+							&sidebar{},
+						),
+						app.Div().Class("column").Body(
+							&articleList{},
+						),
+					),
+				),
+			),
+		),
+	)
+}
